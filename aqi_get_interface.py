c5c12dda343733d52f927e05be9222f7031c032d
quarkov <3dquarkov@gmail.com>
2019-06-19 19:12:31 +0200

    encapsulated into AQI class

--- aqi_get_interface.py
+++ aqi_get_interface.py
@@ -2,55 +2,52 @@ from collections import defaultdict
 import requests
 
 
-stations_token = "http://api.gios.gov.pl/pjp-api/rest/station/findAll"
-sensors_token = "http://api.gios.gov.pl/pjp-api/rest/station/sensors/"
-magnitudes_token = "http://api.gios.gov.pl/pjp-api/rest/data/getData/"
+class AQI:
+    stations_token = "http://api.gios.gov.pl/pjp-api/rest/station/findAll"
+    sensors_token = "http://api.gios.gov.pl/pjp-api/rest/station/sensors/"
+    magnitudes_token = "http://api.gios.gov.pl/pjp-api/rest/data/getData/"
+
+    def __init__(self):
+        self.country_stations = self.get_country_stations()
+        self.cities = self.get_cities_names()
+
+    def get_country_stations(self):
+        response = requests.get(self.stations_token).json()
+        country_stations = defaultdict(dict)
+        for record in response:
+            if record["city"]:
+                city_name = record["city"]["name"]
+                station = {record["id"]: {"address": record["addressStreet"], "data": None}}
+                country_stations[city_name].update(station)
+        return country_stations
+
+    def get_cities_names(self):
+        return sorted(self.country_stations.keys())
+
+    def get_city_stations(self, city):
+        return self.country_stations[city]
+
+    def get_station_data(self, station_id):
+        sensors = requests.get(f"{self.sensors_token}{station_id}").json()
+        sensors_id = [s["id"] for s in sensors]
+        data = {}
+        for id in sensors_id:
+            magnitudes = requests.get(f"{self.magnitudes_token}{id}").json()
+            chems = magnitudes["key"]
+            value = magnitudes["values"][1]["value"]
+            data.update({chems: round(value, 2)})
+        return data
+
+    def get_city_report(self, city_name):
+        city_name = " ".join([chunk.capitalize() for chunk in city_name.split()])
+        if city_name not in self.cities:
+            return "Can't find data for this city. Check spelling and/or try another city"
+
+        for station in self.country_stations[city_name]:
+            self.country_stations[city_name][station]["data"] = self.get_station_data(station)
+        return self.country_stations[city_name]
 
 
-def get_all_stations():
-    return requests.get(stations_token).json()
-
-
-def get_city_names():
-    all_stations = get_all_stations()
-    cities = set()
-    for station in all_stations:
-        if station["city"]: cities |= {station["city"]["name"]}
-    return sorted(list(cities))
-
-
-def get_stations(city_name):
-    all_stations = get_all_stations()
-    stations_id = []
-    for chunk in all_stations:
-        if chunk["city"] and chunk["city"]["name"].lower() == city_name.lower():
-            stations_id.append(chunk["id"])
-    return stations_id
-
-
-def get_sensors(station_id):
-    sensors = requests.get(f"{sensors_token}{station_id}").json()
-    sensors_id = [s["id"] for s in sensors]
-    return sensors_id
-
-
-def get_magnitudes(sensor_id):
-    magnitudes = requests.get(f"{magnitudes_token}{sensor_id}").json()
-    value = magnitudes["values"][0]["value"]
-    if not value: value = magnitudes["values"][1]["value"]
-    return (magnitudes["key"], value) if value else (magnitudes["key"], 0)
-
-
-def get_report(city_name):
-    try:
-        print(city_name)
-        stations = get_stations(city_name)
-        report = defaultdict(int)
-        for station in stations:
-            sensors = get_sensors(station)
-            for sensor in sensors:
-                mol, value = get_magnitudes(sensor)
-                report[mol] = max(report[mol], value)
-        return "\n".join(sorted([f"{k}: {v}" for k, v in report.items()]))
-    except Exception:
-        return "Something went wrong, try again"
+report = AQI()
+gdansk = report.get_city_report("Gdańsk")
+[print(f"{e['address']} {e['data']}") for e in gdansk.values()]
