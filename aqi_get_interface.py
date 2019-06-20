dc981f370c812201d8614e68e88c67ef1567fa15
quarkov <3dquarkov@gmail.com>
2019-06-20 14:50:57 +0200

    short_report and extended_report methods added

--- aqi_get_interface.py
+++ aqi_get_interface.py
@@ -8,8 +8,10 @@ class AQI:
     magnitudes_token = "http://api.gios.gov.pl/pjp-api/rest/data/getData/"
 
     def __init__(self):
-        self.country_stations = self.get_country_stations()
-        self.cities = self.get_cities_names()
+        self._country_stations = self.get_country_stations()
+        self._cities = self.get_cities_names()
+        self._extended_reports = defaultdict(dict)
+        self._short_reports = defaultdict(dict)
 
     def get_country_stations(self):
         response = requests.get(self.stations_token).json()
@@ -22,10 +24,10 @@ class AQI:
         return country_stations
 
     def get_cities_names(self):
-        return sorted(self.country_stations.keys())
+        return sorted(self._country_stations.keys())
 
     def get_city_stations(self, city):
-        return self.country_stations[city]
+        return self._country_stations[city]
 
     def get_station_data(self, station_id):
         sensors = requests.get(f"{self.sensors_token}{station_id}").json()
@@ -34,20 +36,39 @@ class AQI:
         for id in sensors_id:
             magnitudes = requests.get(f"{self.magnitudes_token}{id}").json()
             chems = magnitudes["key"]
-            value = magnitudes["values"][1]["value"]
+            value = 0
+            for m in magnitudes["values"]:
+                if m["value"]:
+                    value = m["value"]
+                    break
             data.update({chems: round(value, 2)})
         return data
 
-    def get_city_report(self, city_name):
+    def get_report_extended(self, city_name):
         city_name = " ".join([chunk.capitalize() for chunk in city_name.split()])
-        if city_name not in self.cities:
+        if city_name not in self._cities:
             return "Can't find data for this city. Check spelling and/or try another city"
 
-        for station in self.country_stations[city_name]:
-            self.country_stations[city_name][station]["data"] = self.get_station_data(station)
-        return self.country_stations[city_name]
+        if city_name not in self._extended_reports:
+            for station in self._country_stations[city_name]:
+                self._country_stations[city_name][station]["data"] = self.get_station_data(station)
+                street = self._country_stations[city_name][station]["address"]
+                values = self._country_stations[city_name][station]["data"]
+                self._extended_reports[city_name].update({street: values})
+        return self._extended_reports[city_name]
+
+    def get_report_short(self, city_name):
+        if city_name in self._short_reports:
+            return self._short_reports[city_name]
+        else:
+            extended = self.get_report_extended(city_name)
+
+        data = defaultdict(dict)
+        for item in extended.values():
+            for mol in item:
+                data[mol] = item[mol] if not data[mol] else max(data[mol], item[mol])
+        self._short_reports[city_name] = data
+        return data
 
 
 report = AQI()
-gdansk = report.get_city_report("Gdańsk")
-[print(f"{e['address']} {e['data']}") for e in gdansk.values()]
